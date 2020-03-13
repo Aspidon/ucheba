b4d87500f93a07caf853b8415ae501c41a116215
Aspidon <aleksandr-stec@mail.ru>
2020-03-13 16:20:23 +0300

    Третий урок, остановился на пол задания

--- Урок 1/js.js
+++ Урок 1/js.js
@@ -1,25 +1,47 @@
+"use strict";
+
 // Урок 1
-let money = +prompt("Ваш бюджет на месяц?", ""),
-    time = prompt("Введите дату в формате YYYY-MM-DD", ""),
-    appData = {
-        budget: money,
-        timeData: time,
-        expenses: {},
-        optionalExpenses: {},
-        income: [],
-        savings: false
-    };
 
-for (let i = 0; i < 2; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-        b = +prompt("Во сколько обойдется?", "");
+let money,
+    time;
 
-    if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && a != '' && b != '' && a.length < 50) {
-        appData.expenses[a] = b;
+function start() {
+    money = +prompt("Ваш бюджет на месяц?", "");
+    while (isNaN(money) || money == "" || money == null) {
+        money = +prompt("Ваш бюджет на месяц?", "");
+    }
+
+    time = prompt("Введите дату в формате YYYY-MM-DD", "");
+}
+
+start();
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+}
+
+function chooseExpenses() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+            b = +prompt("Во сколько обойдется?", "");
+
+        if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null && a != '' && b != '' && a.length < 50) {
+            console.log("done");
+            appData.expenses[a] = b;
+        } else {
+            i = i - 1;
+        }
     }
 }
 
-appData.moneyPerDay = appData.budget / 30;
+chooseExpenses();
+
+appData.moneyPerDay = (appData.budget / 30).toFixed();
 
 console.log(money);
 console.log(time);
