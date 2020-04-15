dd9e1d7e4a422557a7b1f547d5541b46e5075a8e
Aspidon <aleksandr-stec@mail.ru>
2020-04-15 21:15:49 +0300

    15 апреля 2020, домашка 10 задание

---
+++ Урок 1/Задание 10/js/js.js
@@ -0,0 +1,81 @@
+"use strict";
+
+window.addEventListener('DOMContentLoaded', function () {
+
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+
+    hideTabContent(1);
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener('click', function (event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+
+    // Таймер 
+
+    let deadLine = '2020-04-16';
+
+    function getTimeRemaining(endTime) {
+        let t = Date.parse(endTime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minuts = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor(t / 1000 / 60 / 60);
+
+        return {
+            'total': t,
+            'seconds': seconds,
+            'minuts': minuts,
+            'hours': hours
+        };
+    }
+
+    function setClock(id, endTime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minuts = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endTime);
+            hours.textContent = t.hours;
+            minuts.textContent = t.minuts;
+            seconds.textContent = t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval;
+            }
+        }
+    };
+
+    setClock('timer', deadLine);
+
+
+
+
+
+});
